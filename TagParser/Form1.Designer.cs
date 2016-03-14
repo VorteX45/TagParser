af6a5dcbecf057f21528b840d3322e611571cdb3
VorteX45 <VorteX456@Rambler.ru>
2016-03-15 02:42:03 +0300

    Работает клонирование и удаление репозитория Работает получение списка тэгов

---
+++ TagParser/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TagParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxDebug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(503, 6);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(102, 24);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Обработка";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ссылка на репозиторий Git:";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAddress.Location = new System.Drawing.Point(205, 7);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(292, 22);
+            this.textBoxAddress.TabIndex = 2;
+            this.textBoxAddress.Text = "https://github.com/developit/preact.git";
+            // 
+            // textBoxDebug
+            // 
+            this.textBoxDebug.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDebug.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDebug.Location = new System.Drawing.Point(12, 43);
+            this.textBoxDebug.Multiline = true;
+            this.textBoxDebug.Name = "textBoxDebug";
+            this.textBoxDebug.Size = new System.Drawing.Size(593, 519);
+            this.textBoxDebug.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 574);
+            this.Controls.Add(this.textBoxDebug);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Git Tag Parser v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxDebug;
+    }
+}
+
---
+++ TagParser/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TagParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process commandLineProcess = new System.Diagnostics.Process();
+            commandLineProcess.StartInfo.UseShellExecute = false;
+            commandLineProcess.StartInfo.RedirectStandardOutput = true;
+            commandLineProcess.StartInfo.WorkingDirectory = Application.StartupPath;
+            StreamReader outputReader;
+            List<string> tagList = new List<string>();
+            const string TEMP_FOLDER_PATH = "repository";
+
+            //клонируем во временную папку
+            commandLineProcess.StartInfo.FileName = "git";
+            commandLineProcess.StartInfo.Arguments = "clone " + textBoxAddress.Text + " " + TEMP_FOLDER_PATH;
+            commandLineProcess.Start();
+            commandLineProcess.WaitForExit();
+            commandLineProcess.Close();
+
+            //получаем список тегов
+            commandLineProcess.StartInfo.WorkingDirectory = Application.StartupPath + "\\" + TEMP_FOLDER_PATH;
+
+            commandLineProcess.StartInfo.FileName = "git";
+            commandLineProcess.StartInfo.Arguments = "tag";
+            commandLineProcess.Start();
+            outputReader = commandLineProcess.StandardOutput;
+            commandLineProcess.WaitForExit();
+            commandLineProcess.Close();
+            while (outputReader.Peek() != -1)
+            {
+                tagList.Add(outputReader.ReadLine());
+                textBoxDebug.Text += (tagList.Last());
+            }
+
+            //получаем информацию по каждому тэгу
+            //commandLineProcess.StartInfo.FileName = "git";
+            //commandLineProcess.StartInfo.Arguments = "clone " + textBoxAddress.Text;
+            //commandLineProcess.Start();
+            //outputReader = commandLineProcess.StandardOutput;
+            //commandLineProcess.WaitForExit();
+            //commandLineProcess.Close();
+
+            //временная папка больше не нужна
+            foreach (string file in Directory.GetFiles(TEMP_FOLDER_PATH, "*", SearchOption.AllDirectories))
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+            }
+            Directory.Delete(TEMP_FOLDER_PATH, true);
+        }
+    }
+}
